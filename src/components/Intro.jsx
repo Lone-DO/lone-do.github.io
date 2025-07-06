542fbdeebd679fe6b3123ce79006ffd2bfa3e7de
lone-DO <citizin@outlook.com>
2025-07-06 13:59:05 +0200

    Update README and descriptions

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,10 +7,7 @@ const Intro = () => {
 			<i className='App_content-title'>Software Engineer | FrontEnd Architect</i>
 			<i className='App_content-title'>Migration Specialist | Technical Consultant</i>
 			<p>
-				I'm a technology enthusiast & Software Engineer who believes in crafting solutions that
-				helps people's business & personal lives easier. My skill set is adaptable to what fits your
-				needs and gets the job done. My Code is built with both clean practices & modularity, and
-				with the end-user in mind for responsiveness & accessibility.
+				I'm a technology enthusiast and software engineer who believes in crafting solutions that make people's business and personal lives easier. My skill set is adaptable to meet your needs and get the job done. My Code is built with both clean practices & modularity, and with the end-user in mind for responsiveness & accessibility.
 			</p>
 
 			<hr></hr>
--- src/components/projects/appData.js
+++ src/components/projects/appData.js
@@ -11,11 +11,11 @@ const appData = [
 		name: 'Animal Crossing Tunes',
 		headline: 'Second Project, built using EmberCLI & MongoDB',
 		description:
-			"Creative application that brings the daily soundtracks of Animal Crossing back, and now it's here to stay for our daily endeavors unlike never before. Able to choose smoothly between the user's desired Generation, from the hit classics like, original Animal Crossing from GameCube, to City Folk from the WII U, and the latest New Leaf on the 3DS. Built with the MEEN stack, backend API through Mongodb.",
+			"Creative application that brings the daily soundtracks of Animal Crossing back, and now it's here to stay for our daily endeavors, unlike never before. Able to choose smoothly between the user's desired Generation, from the hit classics like original Animal Crossing from GameCube, to City Folk from the WII U, and the latest New Leaf on the 3DS. Built with the MERN stack, the backend API utilizes MongoDB as its database.",
 		launch: ACTunes,
 		github: 'https://github.com/Lone-DO/animalcrossing',
 		modules: [
-			'MEEN Stack',
+			'MERN Stack',
 			'Mongo',
 			'Express',
 			'Ember.js',
@@ -72,7 +72,7 @@ const appData = [
 		name: 'C# Shorts',
 		headline: 'Projects built while studying C# via Treehouse',
 		description:
-			'My small projects, short and sweet using node & C#. After going a year with JavaScript, picking up this language felt natural for what exposure i managed to dabble into.',
+			'My small projects, short and sweet, using Node & C#. After going a year with JavaScript, picking up this language felt natural, given the exposure I managed to dabble in.',
 		launch: 'disabled',
 		github: 'https://github.com/Lone-DO/Treehouse/tree/master/C%23',
 		modules: ['Node.js', 'C#', '.Net'],
@@ -84,7 +84,7 @@ const appData = [
 		name: 'Undertale',
 		headline: 'First project, built using Ember.js',
 		description:
-			"Recreational Project that restructures the original Site to a more update formality. Built using Ember, my first project I've built as a Developer. Complete overhaul of hands-on coding and features that utilize API information for the user's discretion. Allows fans to post 'Mail' on the site for the Undertale Devs to see, and others to respond to. Built with Ember.js, Backend localStorage.",
+			"Recreational Project that restructures the original site into a more updated formality. Built using Ember, my first Project I've built as a Developer. Complete overhaul of hands-on coding and features that utilize API information for the user's discretion. Allows fans to post 'Mail' on the site for the Undertale Devs to see, and others to respond to. Built with Ember.js, Backend localStorage.",
 		launch: 'https://lone-do.github.io/#/undertale/',
 		github: 'https://github.com/Lone-DO/Undertale-Recreation',
 		modules: ['HTML5', 'CSS3', 'JavaScript - ES6', 'Ember.js', 'localStorage', 'SASS', 'jQuery'],
